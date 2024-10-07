1cc3006b63bdf421a8ccdcf9daee2d833d0e2f84
Vero1500 <mirceaoanaveronica@gmail.com>
2024-10-07 20:12:22 +0300

    Initiate angular project
    Installed angular and required modules

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "../app/components/header/header.component";
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  template: ` <app-header></app-header> `,
+  styles: [],
+})
+export class AppComponent {
+  title = "store";
+}
